60e4a6ba26006a611ea78aa31825073a6bed43d2
ThelmaRivas <thelma.rivas00@gmail.com>
2023-11-27 17:19:35 -0600

    establish use and port
    use apiRoutes and htmlRoutes and especify listening port

--- Develop/server.js
+++ Develop/server.js
@@ -3,3 +3,17 @@ const fs = require('fs');
 const path = require('path');
 const htmlRoutes = require('./routes/html/htmlRoutes');
 const apiRoutes = require('./routes/api/apiRoutes'); 
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'))
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
