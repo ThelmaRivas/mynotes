c27e6e15fb76783cae45aa20bcbfa38d41045df1
ThelmaRivas <thelma.rivas00@gmail.com>
2023-11-27 17:06:09 -0600

    Add html routes
    Import modules required

--- Develop/routes/api/apiRoutes.js
+++ Develop/routes/api/apiRoutes.js
@@ -43,7 +43,7 @@ router.post('/notes', (req, res) => {
 });
 
 router.delete('/notes/:id', (req, res) => {
-    // Read the db.json file
+    
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
             console.error(err);
@@ -53,24 +53,24 @@ router.delete('/notes/:id', (req, res) => {
         const notes = JSON.parse(data);
         const noteId = req.params.id;
 
-        // Find the index of the note with the specified ID
+        
         const noteIndex = notes.findIndex(note => note.id === noteId);
 
         if (noteIndex === -1) {
             return res.status(404).json({ error: 'Note not found' });
         }
 
-        // Remove the note from the array
+        
         notes.splice(noteIndex, 1);
 
-        // Write the updated notes array to the db.json file
+        
         fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
             if (err) {
                 console.error(err);
                 return res.status(500).json({ error: 'Internal Server Error' });
             }
 
-            res.sendStatus(204); // Send a success status code indicating successful deletion
+            res.sendStatus(204);
         });
     });
 });
