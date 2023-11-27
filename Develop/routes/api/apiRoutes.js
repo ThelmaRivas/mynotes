9408e10c06b9a1ad3189473a554c09b12e4b32fc
ThelmaRivas <thelma.rivas00@gmail.com>
2023-11-27 16:49:28 -0600

    GET request
    create the get request that will read the db.json file and return all saved notes as JSON

--- Develop/routes/api/apiRoutes.js
+++ Develop/routes/api/apiRoutes.js
@@ -2,3 +2,17 @@ const express = require('express');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
+const router = express.Router();
+
+router.get('/notes', (req, res) => {
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        const notes = JSON.parse(data);
+        res.json(notes);
+    });
+});
